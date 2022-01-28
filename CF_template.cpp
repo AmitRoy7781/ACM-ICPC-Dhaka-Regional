1406b9800c1677cd2a18e6d25006e05b2eaa77f6
Amit Roy <aroy7298@gmail.com>
2022-01-28 21:08:10 +0600

    Add CF Template

---
+++ CF_template.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+ 
+// #include <ext/pb_ds/assoc_container.hpp> // Common file
+// #include <ext/pb_ds/tree_policy.hpp> // Including tree_order_statistics_node_update
+// #include <ext/pb_ds/detail/standard_policies.hpp>
+// using namespace __gnu_pbds;
+ 
+// typedef tree<pair<int,int>,null_type,less<pair<int,int>>,rb_tree_tag,
+// tree_order_statistics_node_update> ordered_set;
+ 
+ 
+typedef long long       ll;
+typedef array<ll, 2>   all;
+typedef array<int, 2>   aii;
+typedef pair<ll, ll>     pll;
+ 
+#define     all(a)  a.begin(), a.end()
+#define     pb      push_back
+#define     ff      first
+#define     ss      second
+ 
+#define     inf     (1000000000)
+#define     infl    (1000000000000000000LL)
+#define     MAX     3007
+#define     MOD     1000000007
+#define     eps     1e-6
+#define     FIN     freopen("input.txt","r",stdin);
+#define     FOUT    freopen("output.txt","w",stdout);
+#define     SET(a)  memset((a), -1, sizeof(a))
+#define     CLR(a)  memset((a), 0, sizeof(a))
+#define     dbg(x)  cout << #x << " = " << x << endl
+ 
+//pos is 0 based
+inline bool checkBit(ll mask, int pos) { return  (bool)(mask & (1ll << pos));}
+inline ll setBit(ll mask, int pos) {    return  (mask | (1ll << pos));}
+inline ll resetBit(ll mask, int pos) {    return  (mask & ~(1ll << pos));}
+inline ll toggleBit(ll mask, int pos) {    return  (mask ^ (1ll << pos));}
+
+void solve()
+{
+	
+}
+
+int main()
+{
+	int tc;
+	cin>>tc;
+
+	while(tc--)
+	{
+		solve();
+	}
+
+}
